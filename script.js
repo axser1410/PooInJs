4e24385dc8ba84cadf52f79d53972234be611ecf
axser1410 <mancillaparedezalexandre@gmail.com>
2023-06-01 00:42:01 -0400

    sistema funcionando y se adiciono el segundo ejercicio(mostrar nombres de equipos de puntaje menor)

--- script.js
+++ script.js
@@ -25,7 +25,34 @@ class EvaluacionProyectosObj { // Definición de la clase 'Persona'
         this.i = [];
         this.filasI = 3
     }
- 
+    
+    mostrar(){
+        console.log(this.carrera, this.universidad, this.nombreObjeto);
+        for (let i = 0; i < this.np; i++) {
+            console.log(this.p[i][0]);//nombreproyecto
+            console.log(this.p[i][1]);//materia
+            console.log(this.p[i][2]);//puntaje
+            console.log(this.i[i][0]);
+            console.log(this.i[i][1]);
+            console.log(this.i[i][2]);  
+        }
+    }
+
+    conseguirMenorPuntaje(){
+        let puntaje = parseInt(this.p[0][2])
+        for (let i = 0; i < this.np; i++) {
+            if (parseInt(this.p[i][2]) <= puntaje) {
+                puntaje = parseInt(this.p[i][2]);
+            }
+        }
+        for (let i = 0; i < this.np; i++) {
+            if (parseInt(this.p[i][2]) === puntaje) {
+                console.log("el nombre del equipo con menor puntaje es "+ this.p[i][0]) //muestra los nombres del equipo
+            }
+            
+        }
+    }
+
     ingresarintegrante1(integrante1) { // Método de instancia
         this.ni[1][this.ni] = integrante1
     }
@@ -34,17 +61,6 @@ class EvaluacionProyectosObj { // Definición de la clase 'Persona'
     }
     ingresarintegrante3(integrante3) { // Método de instancia
         this.ni[3][this.ni] = integrante3
-    }
-    mostrar(){
-        console.log(this.carrera, this.universidad, this.nombreObjeto);
-        for (let i = 0; i < this.np; i++) {
-            console.log(this.p[i][0]);
-            console.log(this.p[i][1]);
-            console.log(this.p[i][2]);
-            console.log(this.i[i][0]);
-            console.log(this.i[i][1]);
-            console.log(this.i[i][2]);  
-        }
     }
 }
 class FeriaAcademica {
@@ -82,6 +98,7 @@ function creaObjEvaluacionDefecto() {
         let b = "Universidad Mayor de San Andres"
         let objetoCrear = new EvaluacionProyectosObj(a, b, entre);
         this.np = 1
+        objetoCrear.ne = 1
         objetoCrear.p[0] = ["Ia", "INFORMATICA", "76"]
         objetoCrear.i[0] = ["Alexandre", "Karen", "Victor"]
         //let objetoCrear = entrada.value;
@@ -104,6 +121,7 @@ function creaObjFeriaDefecto() {
         let c = "12/12/12";
         let objetoCrear = new FeriaAcademica(a, b, c, entre);
         this.np = 1
+        objetoCrear.ne = 1
         objetoCrear.e[0] = ["Inteligencia Artificial", "Umsa"]
         //let objetoCrear = entrada.value;
         objetosEvaluacion.push(objetoCrear)
@@ -144,13 +162,13 @@ function creaObjEvaluacion(){
                     //console.log(valor);
                     proyecto.push(valor)
                     //console.log("funciona hasta5");
-                    console.log(proyecto);
+                    //console.log(proyecto);
                 }
                 for (let m = arregloEvaluacion.length/2; m < arregloEvaluacion.length; m++) {
                     //console.log("ingresa a 2");
                     let valor2 = prompt(arregloEvaluacion[m])
                     integrantes.push(valor2)
-                    console.log(integrantes);
+                    //console.log(integrantes);
                     //console.log("funciona hasta6");
                 }
             }
@@ -158,11 +176,13 @@ function creaObjEvaluacion(){
             objetoCrear.i.push(integrantes)
             resp = prompt("desea seguir agregando?   si  o  no")
         }
+        objetoCrear.np = auxnp
         //let objetoCrear = entrada.value;
         objetosEvaluacion.push(objetoCrear)
         objetosGeneral.push(objetoCrear)
         console.log(objetosEvaluacion); 
         entrada.value = ""
+        
     }
     else{
         alert("ingrese un nombre de la variable para crear el objeto o cambie el nombre de la variable")
@@ -178,7 +198,6 @@ function creaObjFeria() {
         let objetoCrear = new FeriaAcademica(a, b, c, entre)
         resp = prompt("desea agregar algun proyecto?   (si/no)")
         while(resp === "si"){
-            console.log("si entra");
             let entradas = []
             auxnp = auxnp + 1
             this.ne = auxnp
@@ -194,6 +213,7 @@ function creaObjFeria() {
 
         }
         //let objetoCrear = entrada.value
+        objetoCrear.ne = auxnp
         objetosFeria.push(objetoCrear)
         objetosGeneral.push(objetoCrear)
         console.log(objetosFeria);
@@ -259,8 +279,14 @@ function func1(){
     }
 }
 
-//function func2() {
-   // let menorPuntaje = objetosEvaluacion[0].conseguirPuntaje()
-//}
+function func2() {
+    let entrada = document.getElementById("nombreBuscar")
+    let valor = entrada.value
+    for (let i = 0; i < objetosEvaluacion.length; i++) { //recorre objetos
+        if (valor === objetosEvaluacion[i].nombreObjeto) {
+            objetosEvaluacion[i].conseguirMenorPuntaje()
+        }
+    }
+}
 
 window.addEventListener("load", revision)
